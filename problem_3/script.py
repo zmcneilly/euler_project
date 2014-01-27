ad9aadccad3cead6b2dfc6fcf412296e5c0b6cba
Zach McNeilly <zmcneilly@elys.com>
2014-01-27 17:13:51 -0500

    Changes for performance (I hope) to problem 3

--- problem_3/script.py
+++ problem_3/script.py
@@ -1,6 +1,7 @@
 #!env/bin/python
 
 nbr = 600851475143
+#nbr = 13195
 primes=[]
 
 def isprime(num):
@@ -15,13 +16,26 @@ def isprime(num):
     primes.append(num)
     return True
 
+def factors(num):
+  MAX = num/2
+  x = 2
+  result=[]
+  while x < MAX:
+    if num % x == 0:
+      for x1 in (factors(num/x)):
+        if x1 not in result:
+          result.append(x1)
+      if x not in result:
+        result.append(x)
+    x += 1
+  return result
+
 result = 1
-MAX = nbr/2
-x = 1
-while x <= MAX:
-  x += 1
+to_check=factors(nbr)
+print "Done Factoring"
+for x in to_check:
   if isprime(x) and x > 0:
-    if nbr % x == 0:
+    if nbr % x == 0 and x > result:
       result = x
 
 print result
