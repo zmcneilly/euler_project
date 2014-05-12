4cb87d03bc347b2a3978e27e351358b277085b99
Zach McNeilly <zach.mcneilly@gmail.com>
2014-05-12 15:43:47 -0400

    Problem 54 is done!
    I decided to go the open source route which is good. Their solution did not provide what was necessary (it calculated the percent chance that a particular hand/board would win in Texas hold'em), but it contained the code that would give me what I needed.

--- problem_54/script.py
+++ problem_54/script.py
@@ -1,6 +1,8 @@
 #!env/bin/python
 
 import helpers as h
+from pokereval.card import Card
+from pokereval.hand_evaluator import HandEvaluator
 
 # http://www.suffecool.net/poker/evaluator.html
 
@@ -24,56 +26,49 @@ import helpers as h
 #Straight Flush: All cards are consecutive values of same suit.
 #Royal Flush: Ten, Jack, Queen, King, Ace, in same suit.
 
+faces = {'T': 10,
+    'J': 11,
+    'Q': 12,
+    'K': 13,
+    'A': 14}
+suits = {'S': 1,
+    'H': 2,
+    'D': 3,
+    'C': 4}
+
 def get_winner(l1,l2):
-    lv1 = [n[0] for n in l1]
-    ls1 = [n[1] for n in l1]
-    results = {}
-    values = {
-        '1' : 13,
-        '2' : 12,
-        '3' : 11,
-        '4' : 10,
-        '5' : 9,
-        '6' : 8,
-        '7' : 7,
-        '8' : 6,
-        '9' : 5,
-        'T' : 4,
-        'J' : 3,
-        'Q' : 2,
-        'K' : 1,
-        'A' : 0}
-    suits = {
-        'C' : 0,
-        'D' : 1,
-        'H' : 2,
-        'S' : 3}
-    for l in [l1,l2]:
-        rf = True
-        fsuit = None
-        sf = True
-        foak = True
-        fh = True
-        f = True
-        s = True
-        toak = True
-        tp = True
-        op = True
-        hc = 0
-        for c in l:
-            cad = list('00000000000000')
-            card[values[c[0]]] = '1'
-            suit = list('0000')
-            suit[suits[c[1]]] = '1'
-            if values[c[0]] > 4:
-                rf = False
-            if fsuit != None and fsuit != c[1]:
-                rf = False
-                f = False
-            elif fsuit == None:
-                fsuit = c[1]
-            if 
-            
+    hands = {'A': [], 'B': []}
+    for card in l1:
+        v = card[0]
+        if v in faces:
+            v = faces[v]
+        else:
+            v = int(v)
+        s = suits[card[1]]
+        hands['A'].append(Card(v,s))
+    for card in l2:
+        v = card[0]
+        if v in faces:
+            v = faces[v]
+        else:
+            v = int(v)
+        s = suits[card[1]]
+        hands['B'].append(Card(v,s))
+
+    evaluator = HandEvaluator.Five
+    rank1 = evaluator.evaluate_rank(hands['A'])
+    rank2 = evaluator.evaluate_rank(hands['B'])
+
+#    hole1 = hands['A'][0:2]
+#    hole2 = hands['B'][0:2]
+#    board1 = hands['A'][2:]
+#    board2 = hands['B'][2:]
+#    score1 = HandEvaluator.evaluate_hand(hole1,board1)
+#    score2 = HandEvaluator.evaluate_hand(hole2,board2)
+    if rank1 < rank2:
+        return 1
+    else:
+        return 0
 
 
 
@@ -81,13 +76,13 @@ def get_winner(l1,l2):
     
 
 f = open('poker.txt','r')
-
+c = 0
 for x in f:
     l = x.replace('\n','').split(' ')
     l1 = l[0:5]
     l2 = l[5:]
-    get_winner(l1,l2)
-
+    c += get_winner(l1,l2)
+print c
 
 f.close()
     
