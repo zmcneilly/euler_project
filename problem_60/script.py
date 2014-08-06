a21a5589ada9d112189d98d7e2c80c359b439637
Zach McNeilly <zach.mcneilly@gmail.com>
2014-08-06 08:32:52 -0400

    Solved problem 60

--- problem_60/script.py
+++ problem_60/script.py
@@ -5,15 +5,15 @@ import primes
 prime_list = primes.PrimeNumbers()
 
 def p60_test(to_check):
-    for __x in to_check:
-        for __y in to_check:
-            if __x == __y:
-                pass
-            elif prime_list.is_prime(int(str(__x)+str(__y))) and \
-                prime_list.is_prime(int(str(__y)+str(__x))):
-                pass
-            else:
-                return False
+    __x = to_check[len(to_check)-1]
+    for __y in to_check:
+        if __x == __y:
+            pass
+        elif prime_list.is_prime(int(str(__x)+str(__y))) and \
+            prime_list.is_prime(int(str(__y)+str(__x))):
+            pass
+        else:
+            return False
 
     return True
 
@@ -22,25 +22,34 @@ def main():
     low_score = 1000000
     _max = 10000
 
-    for a in xrange(3,_max,2):
-        if not prime_list.is_prime(a):
-            continue
-        for b in xrange(a+2,_max,2):
-            if not prime_list.is_prime(b):
+    plist = []
+
+    for __x in xrange(3,_max,2):
+        if prime_list.is_prime(__x):
+            plist.append(__x)
+
+    for a in plist:
+        for b in plist:
+            if b <= a or not p60_test([a,b]):
                 continue
-            for c in xrange(b+2,_max,2):
-                if not prime_list.is_prime(c):
+            for c in plist:
+                if c <= b or not p60_test([a,b,c]):
                     continue
-                for d in xrange(c+2,_max,2):
-                    if prime_list.is_prime(d):
-                        test_list = [a,b,c,d]
+                for d in plist:
+                    if d <= c or not p60_test([a,b,c,d]):
+                        continue
+                    for e in plist:
+                        if e <= d:
+                            continue
+                        test_list = [a,b,c,d,e]
                         if p60_test(test_list):
                             __s = sum(test_list)
                             if __s < low_score:
                                 print test_list
                                 low_score = __s
+                                print low_score
+                                return 0
 
-    print low_score
 
 if __name__ == '__main__':
     prime_list.load_prime_file()
